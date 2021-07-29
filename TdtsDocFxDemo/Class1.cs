5a9f65da208f3963faac141916f24a46b7a9bdbe
Andy <andy@ingenuitynow.net>
2021-07-28 21:50:35 -0400

    Add project files.

---
+++ TdtsDocFxDemo/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TdtsDocFxDemo
+{
+    /// <summary>
+    /// A fully functioning calculator
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// This great method adds two integers
+        /// </summary>
+        /// <param name="a">the first int</param>
+        /// <param name="b">the second int</param>
+        /// <returns>a magically calculated value</returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// This great method subracts two integers
+        /// </summary>
+        /// <param name="a">the first int</param>
+        /// <param name="b">the second int</param>
+        /// <returns>a magically calculated value</returns>
+        public int Subtract(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ TdtsDocFxDemo/articles/intro.md
@@ -0,0 +1,10 @@
+# Add your introductions here!
+
+Welcome to the Calculator 2000
+Solving all your calculation needs
+
+## more info
+
+- one
+- two 
+- three
